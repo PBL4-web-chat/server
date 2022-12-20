87699c26dc1cf4fe2610cce6013901276831bade
TRAN MINH NHAT <76552355+Azanamin@users.noreply.github.com>
2022-12-20 16:53:27 +0700

    change auth data

--- routes/auth.js
+++ routes/auth.js
@@ -50,7 +50,13 @@ router.post('/login', async(req, res) => {
                 .json({ success: false, message: "user not existed" });
 
         if(await argon2.verify(user.password, password))
-            return res.status(200).json({ success: true, message: "authenticated", access_token: user._id });
+            return res.status(200).json({ 
+                success: true, 
+                message: "authenticated", 
+                data: {
+                    access_token: user._id,
+                    username: username
+                } });
         else
             return res.status(400).json({ success: false, message: "incorrect password" });
     } catch(err) {
